85233e0e5081735e6f3a482c066430e2503a6812
Caleo <caleo@Caleos-MacBook-Pro.local>
2018-12-19 22:32:30 -0500

    fix form 03

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,9 +37,25 @@ const IndexPage = (props) => {
           </svg>
         </div>
           <WorkScreen isHomePage={true} works={data.showWorkHome.edges} />
-          <form name="contact-test" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-            <input name="name" placeholder="Your Name" type="text" />
-            <button>Send</button>
+          <form name="contacts" method="POST" data-netlify="true">
+            <p>
+              <label>Your Name: <input type="text" name="name" /></label>
+            </p>
+            <p>
+              <label>Your Email: <input type="email" name="email" /></label>
+            </p>
+            <p>
+              <label>Your Role: <select name="role[]" multiple>
+                <option value="leader">Leader</option>
+                <option value="follower">Follower</option>
+              </select></label>
+            </p>
+            <p>
+              <label>Message: <textarea name="message"></textarea></label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
           </form>
       </div>
     </div>
