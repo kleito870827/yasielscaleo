a08629106068b1507a9ad9e70f55c8ef4b4a146e
Caleo <caleo@Caleos-MacBook-Pro.local>
2019-01-07 21:52:25 -0500

    not svg

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { IoMdRocket } from "react-icons/io";
 
 // Components
 import Social from './Social'
-// import Contact from './Contact'
+import Contact from './Contact'
 
 class Footer extends Component {
   state= {
@@ -19,25 +19,26 @@ class Footer extends Component {
   }
   render(){
     return (
-      <div className="footer">
-
+      <footer className="footer">
+        <Contact />
         <Social />
         <div className="footer__bottom">
-          <svg className="diagonal home-left" width="80%" height="70" viewBox="0 0 100 102" preserveAspectRatio="none">
-          <path d="M0 100 L100 100 L0 10 Z"></path>
-          </svg>
-          <svg className="diagonal home-right" width="20%" height="70" viewBox="0 0 100 102" preserveAspectRatio="none">
-          <path d="M0 100 L100 100 L100 10 Z"></path>
-          </svg>
+
           <div className="footer__bottom__back-top" onClick={this.onClickBackTop}>
             <IoMdRocket className="footer__bottom__back-top__icon" />
             <p className="footer__bottom__back-top__text">Go to the Space</p>
           </div>
           <p className="footer__bottom__copyright">© {this.state.date.getFullYear()} Yasiel S Caleo</p>
         </div>
-      </div>
+      </footer>
     )
   }
 }
+// <svg className="diagonal home-left" width="80%" height="70" viewBox="0 0 100 102" preserveAspectRatio="none">
+// <path d="M0 100 L100 100 L0 10 Z"></path>
+// </svg>
+// <svg className="diagonal home-right" width="20%" height="70" viewBox="0 0 100 102" preserveAspectRatio="none">
+// <path d="M0 100 L100 100 L100 10 Z"></path>
+// </svg>
 
 export default Footer;
